9b2863c907b4a7706dd99e06e01ec4ae54b95100
Gergő Marcell Turi <gergo.marcell.turi@gmail.com>
2020-05-14 17:47:29 +0200

    bigger primes and stuff

--- src/main/java/RSA.java
+++ src/main/java/RSA.java
@@ -12,8 +12,8 @@ public class RSA extends RSAUtilities {
 
     public RSA() {
         Logger.info("Generating primes...");
-        this.p = generatePrime(10, 100000);
-        this.q = generatePrime(10, 100000);
+        this.p = generatePrime(10, 1000000000);
+        this.q = generatePrime(10, 1000000000);
         Initialization();
     }
 
@@ -26,17 +26,11 @@ public class RSA extends RSAUtilities {
 
     private void Initialization() {
         Logger.info("Primes generated: \n p: {}\n q: {}", this.p, this.q);
-
-        Logger.info("Generating phi...");
         this.phi = (p-1)*(q-1);
         Logger.info("Phi generated: {}", this.phi);
-
-        Logger.info("Generating Public Key....");
         this.publicKey = new PublicKey(p*q, generateVariableE(this.phi));
         Logger.info("Public Key generated: \n e: {}\n n: {}",
                 this.publicKey.getE(),this.publicKey.getN() );
-
-        Logger.info("Generating Private key...");
         this.privateKey = new PrivateKey(EEA(phi, this.publicKey.getE()));
         Logger.info("Private Key generated: {}", this.privateKey.getD());
     }
